4b9262a507419998bde3315de4cbde7763c33739
pessoadavi <pessoadavi@gmail.com>
2023-12-07 15:09:32 -0300

    criação tags da tabela.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
-import { Header } from "../../components/Header";
 import Summary from "../../components/Summary";
+import { Header } from "../../components/Header";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export const Transactions = () => {
     return (
         <>
             <Header />
-            <Summary/>
+            <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%" >Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$12.500,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>07/12/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%" >Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$12.500,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>07/12/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </>
     );
 }
