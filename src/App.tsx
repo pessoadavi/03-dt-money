daea78c293f18621271d4aacbeadfcc64bb51896
pessoadavi <pessoadavi@gmail.com>
2023-12-07 11:07:31 -0300

    inicialização do pronto, instalado styled-components, @types/styled-components e implementando os contextos globais.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/default"
+import { GlobalStyle } from "./styles/global"
+
+export function App() {
+
+  return (
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle/>
+        <h1>Oi</h1>
+      </ThemeProvider>
+  )
+}
