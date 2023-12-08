d859af3a7e5bd05d882aca229e49995348887b4b
pessoadavi <pessoadavi@gmail.com>
2023-12-08 10:09:31 -0300

    iniciando a utilização da lib radix.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import logoimg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export const Header = () => {
@@ -10,9 +11,21 @@ export const Header = () => {
                         src={logoimg}
                         alt=''
                     />
-                    <NewTransactionButton>
-                        Nova transação
-                    </NewTransactionButton>
+                    <Dialog.Root>
+                        <Dialog.Trigger asChild>
+                            <NewTransactionButton>
+                                Nova transação
+                            </NewTransactionButton>
+                        </Dialog.Trigger>
+
+                        <Dialog.Portal>
+                            <Dialog.Overlay />
+                            <Dialog.Content>
+                                <Dialog.Title> Nova transação</Dialog.Title >
+                                <Dialog.Close />
+                            </Dialog.Content>
+                        </Dialog.Portal>
+                    </Dialog.Root>
                 </HeaderContent>
             </HeaderContainer>
         </>
